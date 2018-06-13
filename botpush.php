995891837e20ce19190bec7e05b83e12491d2c94
jebjai <36148709+jebjai@users.noreply.github.com>
2018-06-13 11:08:11 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'qkKOoluNm+dSDq0hfd5gx2fW8ik3+oB24c6tpJd55Myww2NncmhXS/8W5JbT/Af9K1dm/y/DdLdLm30nPOtTgdCvmWnozKtTixPplw5KZMCcY/HjQpU+2r/rWYPS8Bl24Sc7kedv8lTZoFG32g+8QQdB04t89/1O/w1cDnyilFU=';
+$access_token = '';
 
-$channelSecret = '26519486e91fbecda43ae73f4a90a7ac';
+$channelSecret = '';
 
-$pushID = 'U74fab1095de622784ca6129b7ccde06c';
+$pushID = '';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
